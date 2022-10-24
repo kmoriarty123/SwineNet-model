4bbc362da479bfcaf7986e732ba37bb0bc12ccac
U-STI\morika <morika@TPH-L17108.sti.local>
2022-10-25 00:43:09 +0200

    added comments

--- code/preprocess_data.py
+++ code/preprocess_data.py
@@ -2,6 +2,17 @@
 
 Creates farm_lists and farm_dicts and saves all the networks in preprocessed format for each year
 
+Requires:
+'../data/agis_data_lim.csv'
+'../data/tour_network.csv'
+'../data/geo_network.csv'
+With Data Structure:
+agis_data_lim: "year" "tvd_nr" "gde_nr" "gde_name" "is_pig_stall" "holding_cat" "tot_pigs" "sows_boar_ind"
+    "idx_weight_PRRS" "idx_weight_ASF" "idx_weight_APP" "idx_weight_PRRS_low" "idx_weight_ASF_low"
+    "idx_weight_APP_low"  "idx_weight_PRRS_med" "idx_weight_ASF_med"  "idx_weight_APP_med"
+tour_network:"tvd_source" "tvd_dest" "event_date" "n_pigs" "contact_type" "tour_id"
+geo_network: "tvd_source" "tvd_dest" "dist" "contact_type"
+
 """
 
 import preprocess_functions as prep
